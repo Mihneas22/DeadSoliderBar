613cde8fe5f40d210a242c000ce8a58bda890ef5
Mihneas22 <mihnea.serban06@gmail.com>
2025-01-26 21:34:32 +0200

    update

---
+++ Domain/Models/ContactForm.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Models
+{
+    public class ContactForm
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public string? Name { get; set; }
+
+        public string? Email { get; set; }
+
+        public string? Text { get; set; }
+
+        public DateTime? DateTime { get; set; }
+    }
+}
